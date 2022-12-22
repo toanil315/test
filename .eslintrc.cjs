b1b5ed5e77849c29e293224bafd69009ea23d00f
Le Anh Hao <hao.le-anh@internal.banvien.com>
2022-12-22 13:09:56 +0700

    setup eslint with prettier, stuck in conflict

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,40 +1,37 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-  parser: '@typescript-eslint/parser',
-	extends: [
-    'xo',
-    "plugin:@typescript-eslint/recommended",
-    // 'prettier/@typescript-eslint',  //"prettier/@typescript-eslint" has been merged into "prettier" in eslint-config-prettier 8.0.0
-		'plugin:react/recommended',
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "xo",
+        "plugin:prettier/recommended",
+        'eslint:recommended',
+        "plugin:@typescript-eslint/recommended",
 
-    "prettier",
-    "plugin:prettier/recommended",
-	],
-	overrides: [
-		{
-			extends: [
-				'xo-typescript',
-			],
-			files: [
-				'*.ts',
-				'*.tsx',
-			],
-		},
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: [
-    "prettier",
-		'react',
-	],
-	rules: {
-    "prettier/prettier": ["error"], // any prettier formatting error is considered as an ESLint error.
-    "react/react-in-jsx-scope": "off",
-
-	},
-};
+        "prettier",
+    ],
+    "overrides": [
+        {
+            "extends": [
+                "xo-typescript"
+            ],
+            "files": [
+                "*.ts",
+                "*.tsx"
+            ]
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "prettier"
+    ],
+    "rules": {
+      "prettier/prettier": "error"
+    }
+}
