d11298c209532cda9829e135c7f356a4d1d65184
Le Anh Hao <hao.le-anh@internal.banvien.com>
2022-12-28 16:10:05 +0700

    config eslint for the storybook

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { type ComponentStory, type ComponentMeta } from '@storybook/react';
 
 import { Button } from './Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
+const Component: ComponentMeta<typeof Button> = {
   title: 'Example/Button',
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} as ComponentMeta<typeof Button>;
+};
+export default Component;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
